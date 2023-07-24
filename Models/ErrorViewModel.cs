e5a3bc3db91fbff2392bcd9211bbdbb43686a015
Enzo Tello <enzotello0@gmail.com>
2023-07-24 18:59:15 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyectoasp.netmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyectoasp.netmvc.Models
+{
+    public class Producto
+    {
+        private long id;
+        private string titulo;
+        private string descripcion;
+        private string cantidadStock;
+
+        [DisplayName("Id auto generado")]
+        public long Id { get => id; set => id = value; }
+
+        [DisplayName ("Titulo del producto")]
+        [Required]
+
+        public string Titulo { get => titulo; set => titulo = value; }
+
+        [DisplayName("description")]
+        [Required]
+        [StringLength(100)]
+
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string CantidadStock { get => cantidadStock; set => cantidadStock = value; }
+    }
+}
